c358499bc4cf57e3ae7eb768642f7c68bebee9fa
killerarz <milov52@gmail.com>
2023-03-15 09:39:19 +0300

    register user model in admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User as CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+admin.site.register(CustomUser)

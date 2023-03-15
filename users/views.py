d4691431dfab72c1f417f48681c78bc99749773e
killerarz <milov52@gmail.com>
2023-03-15 22:09:10 +0300

    add user registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from .forms import CreationForm
+
 
 # Create your views here.
+
+class SignUp(CreateView):
+    form_class = CreationForm
+
+    success_url = reverse_lazy('meal_app:index')
+    template_name = 'users/signup.html'

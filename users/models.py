a61e7e924ab951d8da1168ca4461c79cf8a25f96
killerarz <milov52@gmail.com>
2023-03-15 10:23:14 +0300

    update user model

--- users/models.py
+++ users/models.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    plan = models.IntegerField(max_length=10)
-    subscription = models.IntegerField(max_length=10)
+    plan = models.IntegerField(default=1)
+    subscription = models.IntegerField(default=1)
 
     def __str__(self):
         return self.username
